ad28faea4d3014b76fa9dad7261069ba8aed8a22
Magnus Poppe Wang <Magnus.Poppe.Wang@gmail.com>
2018-05-26 15:55:44 +0200

    Hidden markov models

--- hmm.py
+++ hmm.py
@@ -1,15 +1,40 @@
-#%%
+# %%
 import numpy as np
 
-### PART 1: Describe the “Umbrella domain” as an HMM: 
+# Transition model for state_t (Answer to to PART A, 1)
+Xt = np.array([[0.7, 0.3], [0.3, 0.7]])
 
-#%%
-# Transition model for state_t 
-X_t = np.array([[0.7, 0.3], [0.3, 0.7]])
+# Sensor model for state_t (Answer to PART A, 2)
+O1 = np.array([[0.9, .0], [.0, 0.2]])
+O3 = np.array([[0.1, .0], [.0, 0.8]])
 
-# Sensor model for state_t
-E_1 = np.array([[0.9, 0], [0, 0.2]])
-E_3 = np.array([[0.1, 0], [0, 0.8]])
 
-print("Transition model:\n", X_t)
-print("Sensor model \n", E_1, "\n", E_3)
+init = np.array([0.5, 0.5])
+
+
+def forward(f, Xt, OT, OF, E, k):
+    t = Xt.transpose().dot(f)        # Transition
+    u = (OT if E[k] else OF).dot(t)  # Update
+    delta = u / np.sum(u)            # Normalize
+
+    # Day 0 (base case)?
+    if not k:
+        return delta
+    return forward(delta, Xt, OT, OF, E, k-1)
+
+def backward(Xt, OT, OF, E, k):
+    e = (OT if E[k] else OF)
+    if k < len(E)-1:
+        res = Xt.dot(e).dot(backward(Xt, OT, OF, E, k+1))
+    else: 
+        res = Xt.dot(e).dot(np.array([1, 1]))
+    
+    return res / np.sum(res)
+
+E = [True, True]
+rain_day_2 = forward(init, Xt, O1, O3, E, len(E)-1)
+print("Probability of rain on day 2 using forward:  ", rain_day_2)
+
+E = np.array([True, True, False, True, True]) 
+print("Probability of rain on day 5 using forward:  ", forward(init, Xt, O1, O3, E, len(E)-1))
+print("Probability of rain on day 2 using backward: ", backward(Xt, O1, O3, E, 0)) 
